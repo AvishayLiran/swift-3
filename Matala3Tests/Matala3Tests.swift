f956a1994f9fe8ea2ee94a7629ae5198a3c02324
Liran Avishay <Liran@Lirans-MacBook-Pro.local>
2016-12-22 13:34:27 +0200

    Initial Commit

---
+++ Matala3Tests/Matala3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Matala3Tests.swift
+//  Matala3Tests
+//
+//  Created by Liran Avishay on 12/22/16.
+//  Copyright © 2016 Liran Avishay. All rights reserved.
+//
+
+import XCTest
+@testable import Matala3
+
+class Matala3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
